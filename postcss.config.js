fb9a43db2ad109b2a6a57f689cac6dbacb7eda0a
Akhil-Techsoft <techsofttest@gmail.com>
2025-08-25 10:10:10 +0530

    Purged style.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import purgecss from '@fullhuman/postcss-purgecss'
+
 export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-};
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+    purgecss.default({
+      content: [
+        './resources/views/**/*.php',
+        './resources/js/**/*.js'
+      ],
+      safelist: ['active', /^btn-/, /^nav-/],
+    })
+  ]
+}
--- resources/views/partials/apps.blade.php
+++ resources/views/partials/apps.blade.php
@@ -31,7 +31,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="{{asset('assets/css/app.min.css')}}" />
         <link rel="stylesheet" href="{{asset('assets/css/fontawesome.min.css')}}" />
-        <link rel="stylesheet" href="{{asset('assets/css/style.css')}}?v1.2" />
+        <link rel="stylesheet" href="{{asset('assets/css/style.min.css')}}?v1.2" />
 		    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}?v1" />
 		<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
