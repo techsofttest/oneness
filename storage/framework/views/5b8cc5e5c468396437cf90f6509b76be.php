d2051f856542bc08f347d4178c5a493b8d0f6dfc
Akhil-Techsoft <techsofttest@gmail.com>
2025-08-27 16:34:54 +0530

    Ignore Laravel compiled/cache/session files

--- storage/framework/views/5b8cc5e5c468396437cf90f6509b76be.php
+++
@@ -1,7 +0,0 @@
-<li <?php if(isset($item['id'])): ?> id="<?php echo e($item['id']); ?>" <?php endif; ?> class="nav-header <?php echo e($item['class'] ?? ''); ?>">
-
-    <?php echo e(is_string($item) ? $item : $item['header']); ?>
-
-
-</li>
-<?php /**PATH E:\xampp\htdocs\oneness_homeo\vendor\jeroennoten\laravel-adminlte\src/../resources/views/partials/sidebar/menu-item-header.blade.php ENDPATH**/ ?>
